12841e5276e1ca2b6f6cfaf88d831d72ebde952d
Lanonymous <ethanweirich31@gmail.com>
2025-02-21 12:22:05 +0100

    Update the project to the latest exiled version.

--- SCP Elevator Interaction/SCPElevatorInteractionPlugin.cs
+++ SCP Elevator Interaction/SCPElevatorInteractionPlugin.cs
@@ -7,10 +7,10 @@ namespace SCPElevatorInteraction
 {
     public class SCPElevatorInteractionPlugin : Plugin<Config>
     {
-        public override string Author => "Lanonymous";
+        public override string Author => "Sexy Lanonymous";
         public override string Name => "SCPElevatorInteraction";
         public override string Prefix => "scpelevatorinteraction";
-        public override Version RequiredExiledVersion => new Version(3, 0, 0);
+        public override Version RequiredExiledVersion => new Version(9, 5, 0);
         public override Version Version => new Version(1, 0, 2);
 
         private EventHandler eventHandler;
